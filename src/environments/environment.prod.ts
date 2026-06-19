acfc1cec90a3193293a216783f0fc3e11aa4dc41
edgardom13 <edgardomontanoolucion@gmail.com>
2026-06-19 14:42:24 -0500

    Initial commit - LandingSaaS project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+  supabase: {
+    url: 'https://ymtvbxwtvrrtewznmqib.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltdHZieHd0dnJydGV3em5tcWliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MjcyOTQsImV4cCI6MjA5NzEwMzI5NH0.CRW1-LGOb8BTVctXHSwvB0ogzGfikfZ3rV3uFEnVUTk'
+  }
 };
